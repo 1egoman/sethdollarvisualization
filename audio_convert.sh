91986417e6bfc723a5636f0d18199acf61b30924
Ryan Gaus <rsg1egoman@gmail.com>
2016-10-08 18:26:22 -0400

    code: command for peter

---
+++ audio_convert.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# vim: set syntax=sh :
+
+# syntax: ./audio_convert input.txt total.wav
+# input.txt:
+# some_phrase
+# 01 02
+
+NAME="$(cat $1 | head -n 1)"
+CONTENTS="$(cat $1 | tail -n +2)"
+
+IFS=$'\n'       # make newlines the only separator
+VAR=0
+for i in $CONTENTS; do
+  CMD="sox $2 "$NAME-$VAR.wav" trim $i"
+  echo "Running $CMD"
+  $CMD
+  VAR=$((VAR+1))
+done
+
